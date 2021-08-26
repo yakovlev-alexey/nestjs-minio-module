6aaf04932dd02efe1664a26db43ea0892db657f7
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2021-08-26 14:56:17 +0300

    feat: make options a generic param to minio service

--- src/minio.service.ts
+++ src/minio.service.ts
@@ -5,8 +5,8 @@ import type { ClientOptions } from "minio";
 
 import { MINIO_CONFIG } from "./constants";
 
-export class MinioService extends Client {
-  constructor(@Inject(MINIO_CONFIG) protected readonly options: ClientOptions) {
+export class MinioService<T extends ClientOptions> extends Client {
+  constructor(@Inject(MINIO_CONFIG) protected readonly options: T) {
     super(options);
   }
 }
